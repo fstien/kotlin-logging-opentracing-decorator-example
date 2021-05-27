ca641a462101cd13b087e8eed73aa8a25e048706
fs-throw-away <francois.stiennon@gmail.com>
2021-05-27 18:11:44 +0100

    Bump to 0.1.4

--- src/EarthquakeClient.kt
+++ src/EarthquakeClient.kt
@@ -1,7 +1,7 @@
 package com.github.fstien
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
-import com.github.fstien.kotlin.logging.opentracing.decorator.withOpenTracingLogs
+import com.github.fstien.kotlin.logging.opentracing.decorator.Logging
 import com.zopa.ktor.opentracing.OpenTracingClient
 import com.zopa.ktor.opentracing.span
 import io.ktor.client.*
@@ -17,7 +17,7 @@ import java.time.LocalDateTime
 import java.util.*
 
 
-private val logger = KotlinLogging.logger {}.withOpenTracingLogs()
+private val logger = Logging.logger {}
 
 class EarthquakeClient {
     val client = HttpClient(Apache) {
